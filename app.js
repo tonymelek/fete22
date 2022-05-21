3ef3d7cf69eb9ca60b1896188aad087c732f8da6
tonymelek <66405901+tonymelek@users.noreply.github.com>
2022-05-21 22:37:35 +1000

    exclude orders pickup now

--- app.js
+++ app.js
@@ -35,37 +35,40 @@ const app = {
     },
     methods: {
         cleanData() {
-            this.cleanedOrders = JSON.parse(this.orders).map(order => {
-                const cleanedOrder = {}
-                for (let key in order) {
-                    if (requiredKeys.includes(key) || key.match(/product_id_[\d]+/)) {
-                        if (key.includes('payment_pickupday')) {
-                            const dateArray = order.payment_pickupday.split(' ')[1].split('.');
-                            const time = parseInt(order.payment_pickuptime.split(':')[0]);
-                            const cleanedOrderIsoDate = `${dateArray[2]}-${dateArray[1]}-${dateArray[0]}T${time < 6 ? time + 12 : time}:00:00.000Z`
-                            cleanedOrder.isodate = cleanedOrderIsoDate;
-                            cleanedOrder.date = new Date(cleanedOrderIsoDate);
-                            cleanedOrder.time = new Date(cleanedOrderIsoDate).getTime();
+            this.cleanedOrders = JSON.parse(this.orders)
+                .filter(order => !['Cancelled', 'Completed'].includes(order.orderstatus_name))
+                .filter(order => order.payment_pickupday !== 'Now')
+                .map(order => {
+                    const cleanedOrder = {}
+                    for (let key in order) {
+                        if (requiredKeys.includes(key) || key.match(/product_id_[\d]+/)) {
+                            if (key.includes('payment_pickupday')) {
+                                const dateArray = order.payment_pickupday.split(' ')[1].split('.');
+                                const time = parseInt(order.payment_pickuptime.split(':')[0]);
+                                const cleanedOrderIsoDate = `${dateArray[2]}-${dateArray[1]}-${dateArray[0]}T${time < 6 ? time + 12 : time}:00:00.000Z`
+                                cleanedOrder.isodate = cleanedOrderIsoDate;
+                                cleanedOrder.date = new Date(cleanedOrderIsoDate);
+                                cleanedOrder.time = new Date(cleanedOrderIsoDate).getTime();
 
-                        } else if (key.match(/product_id_[\d]+/)) {
-                            const product_num = key.split('_')[(key.split('_').length - 1)];
-                            if (!cleanedOrder.orderItems) cleanedOrder.orderItems = []
-                            if (order[`product_name_${product_num}`] !== '')
-                                cleanedOrder.orderItems.push({
-                                    name: order[`product_name_${product_num}`],
-                                    options: this.cleanOption(order[`product_options_${product_num}`]),
-                                    quantity: order[`product_quantity_${product_num}`],
-                                    total: order[`product_total_with_tax_${product_num}`],
-                                });
+                            } else if (key.match(/product_id_[\d]+/)) {
+                                const product_num = key.split('_')[(key.split('_').length - 1)];
+                                if (!cleanedOrder.orderItems) cleanedOrder.orderItems = []
+                                if (order[`product_name_${product_num}`] !== '')
+                                    cleanedOrder.orderItems.push({
+                                        name: order[`product_name_${product_num}`],
+                                        options: this.cleanOption(order[`product_options_${product_num}`]),
+                                        quantity: order[`product_quantity_${product_num}`],
+                                        total: order[`product_total_with_tax_${product_num}`],
+                                    });
+                            }
+                            else {
+                                cleanedOrder[key] = order[key]
+                            }
                         }
-                        else {
-                            cleanedOrder[key] = order[key]
-                        }
+
                     }
-
-                }
-                return cleanedOrder;
-            }).filter(order => order.orderstatus_name !== 'Cancelled')
+                    return cleanedOrder;
+                })
                 .sort((a, b) => a.time - b.time)
                 .filter(order => this.displayDates.includes(order.isodate.split('T')[0]))
 
